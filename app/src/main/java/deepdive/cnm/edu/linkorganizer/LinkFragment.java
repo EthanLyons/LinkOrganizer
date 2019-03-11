edf2c4a928db70d092631181d1b0b93b27768c3e
Ethan Lyons <ethan.lyons84@gmail.com>
2019-03-11 16:53:31 -0600

    Fixed Database classes

--- app/src/main/java/deepdive/cnm/edu/linkorganizer/LinkFragment.java
+++ app/src/main/java/deepdive/cnm/edu/linkorganizer/LinkFragment.java
@@ -1,11 +1,16 @@
 package deepdive.cnm.edu.linkorganizer;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import deepdive.cnm.edu.linkorganizer.entity.Link;
+import java.util.List;
 
 
 /**
@@ -13,6 +18,11 @@ import android.view.ViewGroup;
  */
 public class LinkFragment extends Fragment {
 
+  private EditText linkUrl;
+  private EditText descriptorUrl;
+
+  private Button deleteButton;
+  private Button saveLink;
 
   public LinkFragment() {
     // Required empty public constructor
@@ -22,8 +32,33 @@ public class LinkFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_link, container, false);
+    View view = inflater.inflate(R.layout.fragment_link, container, false);
+    linkUrl = view.findViewById(R.id.link_url);
+    descriptorUrl = view.findViewById(R.id.descriptor_url);
+    deleteButton = view.findViewById(R.id.delete_button);
+    saveLink = view.findViewById(R.id.save_link);
+    saveLink.setOnClickListener((v) -> saveLink());
   }
 
+  private void saveLink() {
+
+    String url = linkUrl.getText().toString();
+    String descriptor = descriptorUrl.getText().toString();
+    Link link = new Link();
+    link.setUrl(url);
+    link.setDescriptor(descriptor);
+
+
+  }
+
+  private static class insertLinkTask extends AsyncTask<Link, Void, List<Long>> {
+
+
+    @Override
+    protected List<Long> doInBackground(Link... links) {
+      return LinkOrganizerDB.getInstance().getLinkDao().insert(links);
+    }
+  }
+
+  ;
 }
